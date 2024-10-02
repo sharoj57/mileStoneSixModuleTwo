e0c97081586f7e52253f4fe2b33796a68ae3e57a
sharoj57 <sharojcse@gmail.com>
2024-10-02 12:51:47 +0600

    added find part

---
+++ find.js
@@ -0,0 +1,8 @@
+const numbersOne = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+const finds = numbersOne.find(evn => evn % 2 === 0);
+// console.log (finds)
+
+
+const name = [ 'Mim', 'Oishi', 'Alif', 'Apsara'];
+const myLove = name.find(mlv => mlv.startsWith('A'));
+console.log(myLove)
